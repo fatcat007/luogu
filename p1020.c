27139cd060a4008ec857732720ecf613ffe0ce67
huangqingjia <huangqingjia@tp-link.com.cn>
2017-11-24 10:43:32 +0800

    ac code for p1020

--- p1020.c
+++ p1020.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 
-int f[100000]; /* f[i]表示长度为i的炮弹序列最后一个炮弹的最大高度*/
+int f[100000]; /* f[i]表示长度为i的炮弹序列最后一个炮弹的高度，如果有多个长度为i的炮弹序列，则记录末尾较高的一个*/
 int fl;
 
 int d[100000]; /* d[i]表示第i台设备能打的最高高度*/
@@ -14,36 +14,35 @@ int main()
 {
 	fl=0;
 	dl=0;
-	while(scanf("%d",&t))
+	while(scanf("%d",&t)!=EOF)
 	{
-		i=1;j=fl+1;
+		i=0;j=fl;
 		while(i<j)
 		{
 			m=(i+j)/2;
-			if(t<=f[m]) /* 第m个炮弹足够高，m+1个炮弹可以射下t高度的导弹*/
+			if(f[m]>=t) /* 第m个炮弹足够高，m+1个炮弹可以射下t高度的导弹*/
 				i=m+1;
 			else
 				j=m;
 		}
-		//以t为结尾的炮弹序列，最长应该是i
-		if(t>f[i]) f[i]=t;
-		if(i>fl) fl=i;
+		//[0,i)的序列末尾高度比大于等于t
+		f[i]=t; //更新i位置的高度
+		if(i+1>fl) fl=i+1;
 
-		i=1;j=dl+1;
+		i=0;j=dl;
 		while(i<j)
 		{
 			m=(i+j)/2;
-			if(t>d[m]) /* 第m台设备无法打到导弹t*/
+			if(d[m]<t) /* 第m台设备无法打到导弹t*/
 				i=m+1;
 			else
 				j=m;
 		}
 		//第i台设备可以打到炮弹t
-		if(i>dl) dl=i;
+		if(i+1>dl) dl=i+1;
 		d[i]=t;
 	}
-	printf("%d\r\n",fl);
-	printf("%d\r\n",dl);
-	return 0;	
+	printf("%d\r\n%d",fl,dl);
+	return 0;
 }
 
